4e309796445bdf763d389f54ecbdbd3c27cf1b0f
Jen <jen.lijo@zalando.de>
2018-07-10 15:11:05 +0200

    Add doughnut charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,14 @@ import * as R from 'ramda';
 export class AppComponent {
 
   chart = [];
-  @ViewChild('canvas') canvas: ElementRef;
+  doughnutLeft = [];
+  doughnutRight = [];
 
-  
+  @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('canvas1') canvas1: ElementRef;
+  @ViewChild('canvas2') canvas2: ElementRef;
+
+
   constructor(private _knowledgeBeforeAfter: KnowledgeBeforeAfterService) {}
 
   createRangeLabels() {
@@ -23,37 +28,84 @@ export class AppComponent {
 
   ngOnInit() {
     this._knowledgeBeforeAfter.getData()
-      .subscribe(res => {
-        
-        const chartData = {
-          labels: this.createRangeLabels(),
-          datasets: [{
-            label: '# of Participants',
-            data: res,
-            borderWidth: 1
-          }]
-        };
+    .subscribe(res => {
 
-        const ctx = this.canvas.nativeElement.getContext('2d');
+      const ctx = this.canvas.nativeElement.getContext('2d');
 
-        this.chart = new Chart(ctx, {
-          type: 'horizontalBar',
-          data: chartData,
-          options: {
-            legend: {
-              display: false
-            },
-            scales: {
-              xAxes: [{
-                display: true
-              }],
-              yAxes: [{
-                display: true
-              }]
+      const chartData = {
+        labels: this.createRangeLabels(),
+        datasets: [{
+          label: '# of Participants',
+          data: res,
+          borderWidth: 1
+        }]
+      };
+
+      this.doughnutLeft = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Workers", "Supervisors", "Managers"],
+          datasets: [
+            {
+              label: "% Workers",
+              data: [40, 25, 10],
+              backgroundColor: [
+                "rgba(116, 192, 153)", // Mint
+                "rgba(255, 102, 102)",
+                "rgba(0, 60, 65)" // dark blue
+              ],
+              borderColor: [
+                "rgba(116, 192, 153)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(0, 60, 65)"
+              ],
+              borderWidth: 0
             }
-          }
-        })
+          ]
+        }
+      });
 
+      this.doughnutRight = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Workers", "Supervisors", "Managers"],
+          datasets: [
+            {
+              label: "% Workers",
+              data: [40, 25, 10],
+              backgroundColor: [
+                "rgba(116, 192, 153)", // Mint
+                "rgba(255, 102, 102)",
+                "rgba(0, 60, 65)" // dark blue
+              ],
+              borderColor: [
+                "rgba(116, 192, 153)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(0, 60, 65)"
+              ],
+              borderWidth: 0
+            }
+          ]
+        }
+      });
+
+      this.chart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: chartData,
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }]
+          }
+        }
       })
+    })
   }
 }
